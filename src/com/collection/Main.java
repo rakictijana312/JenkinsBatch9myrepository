d6ff4215546e8a377c248e32865b6cec2748d19d
rakic <rakicijana312@gmail.com>
2021-07-22 21:55:08 -0400

    Tijana Mihailo

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) {
         for(int i=0; i<5; i++){
-            System.out.println("Tijana");
+            System.out.println("Tijana, Mihailo");
         }
     }
 }
